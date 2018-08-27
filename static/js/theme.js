4c347dac872ba09d995880e12223a8e46873a513
Peter Desmet <peter.desmet.work@gmail.com>
2018-08-27 14:09:23 +1200

    Move JS to add bootstrap to theme-sidebar-nav to js file
    That way it is easier to include in satellite websites

---
+++ static/js/theme.js
@@ -0,0 +1,6 @@
+/* Add bootstrap classes to theme-sidebar-nav */
+$(document).ready(function() {
+     $("#theme-sidebar-nav ul").addClass("nav flex-column");
+     $("#theme-sidebar-nav li").addClass("nav-item");
+     $("#theme-sidebar-nav a").addClass("nav-link");
+});
