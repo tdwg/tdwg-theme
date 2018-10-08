857d5290470306deea2dc5446d1cfc4e04dbba8a
Peter Desmet <peter.desmet.work@gmail.com>
2018-10-08 16:37:53 +0200

    Add all Bootstrap classes with jQuery (not @extend)
    @extend adds css, rather than the class to the element. This often doesn't play nice with extending with other classes. E.g. adding .table-dark to an element with @extend .table doesn't work, because the bg of @extend .table will have priority over .table-dark. Adding classes with jQuery might be a bit slower, but it has the advantage that it is easier to debug

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,6 +1,8 @@
-/* Add bootstrap classes to theme-sidebar-nav */
+/* Add bootstrap classes to elements that cannot (always) be controlled in template */
 $(document).ready(function() {
     $("#theme-sidebar-nav ul").addClass("nav flex-column");
     $("#theme-sidebar-nav li").addClass("nav-item");
     $("#theme-sidebar-nav a").addClass("nav-link");
+    $("main.container table").addClass("table table-striped");
+    $("main.container img").addClass("img-fluid");
 });
