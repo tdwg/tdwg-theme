41f2ea33dd854862ce3919d76ddd114285e09151
Peter Desmet <peter.desmet.work@gmail.com>
2018-10-08 16:46:58 +0200

    Also apply .table / .img-fluid for .container-fluid

--- static/js/theme.js
+++ static/js/theme.js
@@ -3,6 +3,10 @@ $(document).ready(function() {
     $("#theme-sidebar-nav ul").addClass("nav flex-column");
     $("#theme-sidebar-nav li").addClass("nav-item");
     $("#theme-sidebar-nav a").addClass("nav-link");
+
     $("main.container table").addClass("table table-striped");
+    $("main.container-fluid table").addClass("table table-striped");
+
     $("main.container img").addClass("img-fluid");
+    $("main.container-fluid img").addClass("img-fluid");
 });
